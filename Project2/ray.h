7adde1e97614fb42eb02364611f8cce292e40b93
JEONG <jungbu2855@unist.ac.kr>
2019-11-28 03:34:24 +0900

    getter setter, 함수 프로토타입 조금

--- Project2/ray.h
+++ Project2/ray.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "vec.h"
 
+#ifndef MAX_RAY_DEPTH
+	#define MAX_RAY_DEPTH 5
+#endif
+
 typedef Vec3<float> Vec3f;
+struct Face;
+struct Light;
 
 class Ray {
 private:
 	Vec3f origin;			// Ray origin
 	Vec3f direction;		// Ray direction vector
 	Vec3f color;			// Ray color, RGB on [0,1]^3
-	float intensity;		// Ray intensity, on [0,1]
+	float intensity;		// Ray intensity, on [0,infinity), default is 1 for light sources, 0 for others
 	int   collisions;		// Number of collisions went through
 public:
 	// Constructors
@@ -34,13 +40,32 @@ public:
 		this->intensity = _ray.intensity;
 		this->collisions = _ray.collisions;
 	}
-	
-	/* Light addition
-	 * It merges two rays, with weighted averaging. */
-	const Ray &operator+ (const Ray &_right) const {
-		
-	}
-	
+
+	// Getters, Setters
+
+	Vec3f getOrigin() const { return origin; }
+	Vec3f getDirection() const { return direction; }
+	Vec3f getColor() const { return color; }
+	float getIntensity() const { return intensity; }
+	int getCollisions() const { return collisions; }
+
+	void setOrigin(const Vec3f &_origin) { origin = _origin; }
+	void setDirection(const Vec3f &_direction) { direction = _direction; }
+	void setColor(const Vec3f &_color) { color = _color; }
+	void setIntensity(float _intensity) { intensity = _intensity; }
+	void setCollisions(int _collisions) { collisions = collisions; }
+
 	// Functions
 
+	// Attenuation
+	void attenuate(const Vec3f &to_pos);
+
+	// Generating a refected ray
+	Ray reflect(const Ray &incident, const Face &face) const;
+
+	// Generating a refracted ray
+	Ray refract(const Ray &incident, const Face &face) const;
+
+	// Generating a shadow ray
+	Ray shadow(const Ray &incident, const Light &light) const;
 };
