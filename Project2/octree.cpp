60a8a23e9767bb19d5c365ed1aa1b406bf980b5e
JEONG <jungbu2855@unist.ac.kr>
2019-12-02 02:18:08 +0900

    octree 시작

--- Project2/octree.cpp
+++ Project2/octree.cpp
@@ -1,4 +1,4 @@
-#include "Octree.h"
+#include "octree.h"
 
 
 
--- Project2/octree.h
+++ Project2/octree.h
@@ -4,9 +4,40 @@
 #include "material.h"
 #include "definitions.h"
 
-class Octree
-{
+typedef unsigned char byte;
+
+/* !!Traversal - parent-parent-(교점이 내부에 존재할 때까지)-parent-child-child-(leaf일 때까지)-child */
+class Octree {
+public:
+	class OctreeNode {
+	private:
+		Face **faceptrs;
+		Vec3f lowest;
+		Vec3f highest;
+		OctreeNode **children;
+		byte index;
+
+	public:
+		bool isLeaf(const OctreeNode *node) const;
+		bool isRoot(const OctreeNode *node) const;
+	};
+private:
+	OctreeNode *root;
+	
 public:
 	Octree();
 	~Octree();
+
+	// Insert, remove
+	void insertAll(const Face **_faceptrs);
+	void remove(const Face *_faceptr);
+
+	// Booleans
+	bool isEmpty() const;
+	
+	// Find
+	const OctreeNode &find(const Face &face) const;
+
+	// Traverse
+
 };
