3dd17b2dce08bf76157124039d2cbf5e5fee6200
JEONG <jungbu2855@unist.ac.kr>
2019-12-02 14:28:35 +0900

    octree 노드 접근 함수 프로토타입

--- Project2/octree.h
+++ Project2/octree.h
@@ -4,22 +4,32 @@
 #include "material.h"
 #include "definitions.h"
 
+#include <vector>
+
 typedef unsigned char byte;
 
-/* !!Traversal - parent-parent-(교점이 내부에 존재할 때까지)-parent-child-child-(leaf일 때까지)-child */
+/* 인접노드 구하기 - parent-parent-(교점이 내부에 존재할 때까지)-parent-child-child-(leaf일 때까지)-child */
 class Octree {
 public:
 	class OctreeNode {
 	private:
-		Face **faceptrs;
-		Vec3f lowest;
-		Vec3f highest;
-		OctreeNode **children;
-		byte index;
+		vector<Face *> faceptrs;	// faces are passed by pointers for mem eff
+		Vec3f lowest;				// lowX lowY lowZ coord
+		Vec3f highest;				// highX highY highZ coord
+		OctreeNode *parent;			// parent pointer
+		OctreeNode **children;		// 8 dynamic child pointers
+		byte index;					// of which side it resides in its parent
 
 	public:
-		bool isLeaf(const OctreeNode *node) const;
-		bool isRoot(const OctreeNode *node) const;
+		bool isLeaf(const OctreeNode *node) const;	// is this node leaf? (no children)
+		bool isRoot(const OctreeNode *node) const;	// is this node root? (no parent)
+
+		OctreeNode *left  () const;		// get left node
+		OctreeNode *right () const;		// get right node
+		OctreeNode *up    () const;		// get up node
+		OctreeNode *down  () const;		// get down node
+		OctreeNode *front () const;		// get front node
+		OctreeNode *back  () const;		// get back node
 	};
 private:
 	OctreeNode *root;
@@ -29,7 +39,7 @@ public:
 	~Octree();
 
 	// Insert, remove
-	void insertAll(const Face **_faceptrs);
+	void insertAll(const Face **_faceptrs, int len);
 	void remove(const Face *_faceptr);
 
 	// Booleans
